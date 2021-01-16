e7157088f7117555ad51219f8586d76e2f320760
Paco Serrano <paco.serrano@logicblox.com>
2021-01-16 18:00:00 +0100

    More changes

--- app/main.py
+++ app/main.py
@@ -4,12 +4,16 @@ from argparse import ArgumentParser
 from flask import json, jsonify,request,session
 from flask_login import current_user, login_user, logout_user, login_required
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import exists
+import sys
+from sqlalchemy import exc,desc,asc
+from  sqlalchemy.sql.expression import func
 
 
 def initDB():
 	db.create_all()
 	db.session.commit()
-
+	'''
 	poolfails = Playlist(name="poolfails")
 	db.session.add(poolfails)
 
@@ -19,6 +23,7 @@ def initDB():
 	db.session.add(user2)
 
 	db.session.commit()
+	'''
 
 def recreateDB():
 	db.drop_all()
@@ -122,11 +127,19 @@ def getPlaylists():
 def getVideos():	
 	playlistId = request.args.get('plid')
 	videos = Video.query.filter(Video.playlists.any(id=playlistId)).all()
-	for video in videos:
-		question = Response.query.filter_by(user_id=current_user.id)
+	#for video in videos:
+	#	question = Response.query.filter_by(user_id=current_user.id)
 		
-
 	return jsonify(videos=[i.to_dict() for i in videos])
+
+# ################################## Get a random video  ####################################
+@app.route('/getRandomVideo', methods=['GET'])
+#@login_required
+def getRandomVideo():
+	# This query will return a random video
+	video = db.session.query(Video).order_by(func.rand()).first() #Answer.query.order_by(func.rand()).first() #asc(Answer.id)).all()
+	print(video.to_dict())
+	return jsonify(video=video.to_dict())
 
 # ################################## Post the response to a question  ####################################
 # Assigns a response to question by a user
@@ -137,28 +150,62 @@ def getVideos():
 @login_required
 def postResponse():
 	data_received = json.loads(request.data)
-	questionId = data_received['questionId']
-	answerId = data_received['answerId']
-	response = Response(user_id=current_user.id, question_id=questionId, answer_id=answerId)
-	db.session.add(response)
+	questionId = int(data_received['questionId'])
+	answerId = int(data_received['answerId'])
+	question = Question.query.filter_by(id=questionId).first()	
+	points = 10
+	if question.official_answer_id == answerId:
+		points = 20
+
+	print(str(points) + ' more points for the user!')
+	try:
+		print("current_user.id: " + str(current_user.id))
+		print("questionId: " + str(questionId))
+		print("answerId: " + str(answerId))
+		user = User.query.filter_by(id=current_user.id).first()
+		user.score += points
+
+		print("Before adding response")
+		# Check if user already answered this question with the same answer
+		# response = Response.query.filter_by(user_id=current_user.id, question_id=questionId, answer_id=answerId).first()
+		responseExists = db.session.query(Response.query.filter(Response.user_id == current_user.id,Response.question_id==questionId,Response.answer_id==answerId).exists()).scalar()
+		response = None
+		if responseExists:
+			# If response exists then increment counter
+			response = Response.query.filter(Response.user_id == current_user.id,Response.question_id==questionId,Response.answer_id==answerId).first()
+			response.counter += 1
+		else:
+			# Create new response
+			response = Response(user_id=current_user.id, question_id=questionId, answer_id=answerId)
+			
+		db.session.add(response)		
+	except exc.IntegrityError:
+		print("This most likely happened because of an integrity error when the user answers the same question more than once")
+		print('user.score: ' + str(user.score))
+		return jsonify(score=user.score, status='500')
+
+	print("Returning correct answer")
+	print('user.score: ' + str(user.score))
+
 	db.session.commit()
-	return {}
+	return jsonify(score=user.score, status='200')
 
 @app.route('/updateVideos', methods=['POST'])
 @login_required
 def updateVideo():
-    data_received = json.loads(request.data)
-    print(data_received)
-    updatedVideosJson = json.loads(data_received['videos'])
-    for updatedVideo in updatedVideosJson:
-    	video = Video.query.filter_by(id=updatedVideo['id']).first()
-    	video.name = updatedVideo['name']
-    	video.youtube_id = updatedVideo['youtubeId']
-    	video.published = bool(updatedVideo['published'])
-    	video.thumbnail = updatedVideo['thumbnail']	
-    	db.session.commit()
+	print("--------------------- INSIDE UPDATE VIDEO")
+	data_received = json.loads(request.data)
+	print(data_received)
+	updatedVideosJson = json.loads(data_received['videos'])
+	for updatedVideo in updatedVideosJson:
+		video = Video.query.filter_by(id=updatedVideo['id']).first()
+		video.name = updatedVideo['name']
+		video.youtube_id = updatedVideo['youtubeId']
+		video.published = bool(updatedVideo['published'])
+		video.thumbnail = updatedVideo['thumbnail']	
+	db.session.commit()
 
-    return {}
+	return {}
 
 ################################### Add a video ####################################
 @app.route('/addVideo', methods=['POST'])
@@ -175,6 +222,7 @@ def addVideo():
 		question = Question(statement=questionJson['statement'], time_to_start=questionJson['time_to_start'],
 					time_to_end=questionJson['time_to_end'], time_to_show=questionJson['time_to_show'], time_to_stop=questionJson['time_to_stop'])
 		answersJson = questionJson['answers']
+		answerIndex = 1
 		for answerJson in answersJson:
 			statement = answerJson
 			answer = Answer.query.filter_by(statement=statement).first()
@@ -183,10 +231,12 @@ def addVideo():
 			if answer is None:
 				answer = Answer(statement=statement)
 
-			if question.official_answer == answer.statement:
+			if int(questionJson['official_answer']) == answerIndex:
+				print("Assigning official answer")
 				question.official_answer = answer
 
 			question.answers.append(answer)
+			answerIndex = answerIndex + 1
 
 		video.questions.append(question)
 
